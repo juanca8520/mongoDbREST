495ef171fc26032abcf3b9dd0177cf57a2d26f2b
Juan Camilo Pulido Gil <juank_pg1998@hotmail.com>
2019-09-12 15:58:08 -0500

    Cambio de mongoose

--- index.js
+++ index.js
@@ -1,34 +1,101 @@
-const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const bodyParser = require('body-parser');
-
+const express = require('express');
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-const dbConfig = require('./config/config.js');
-const mongoose = require('mongoose');
+let db;
 
-mongoose.Promise = global.Promise;
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
-mongoose.set('useFindAndModify', false);
+// Database Name
+const dbName = 'PAISES';
 
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(()=>{
-    console.log('Me conecté a la base de datos');
-}).catch(err => {
-    console.log('Hubo un error', err);
-    process.exit();
-})
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    db = client.db(dbName);
+
+    client.close();
+});
 
 app.get('/', (req, res) => {
-    res.json({"message":"Hola"});
+    res.send({ message: "Funciona" });
 });
 
-require('./app/routes/paises.routes.js')(app);
-
-app.listen(3000, ()=>{
-    console.log("Servidor escuchando en 3000");
+app.listen(port, () => {
+    console.log("Servidor escuchando en ", port);
 });
+
+app.post('/paises', (req, res) => {
+    console.log(req.body);
+    if (!req.body) {
+        return res.status(400).send({
+            message: "No se puede tener un cuerpo vacio"
+        });
+    }
+
+    let pais = {
+        country: req.body.country,
+        population: req.body.population,
+        continent: req.body.continent,
+        lifeExpectancy: req.body.lifeExpectancy,
+        purchasingPower: req.body.purchasingPower
+    }
+
+    const collection = db.collection('pais');
+
+    collection.insertMany([pais], (err, result) => {
+        res.send(result);
+    })
+
+})
+
+app.get('/paises', (req, res) => {
+    const collection = db.collection('pais');
+    collection.find({}).toArray((err, docs) => {
+        res.send(docs);
+    })
+});
+
+app.get('/paises/:paisId', (req, res) => {
+    const collection = db.collection('pais');
+    collection.find({ 'country': req.params.paisId }).toArray((err, docs) => {
+        res.send(docs);
+    })
+});
+
+app.put('/paises/:paisId', (req, res) => {
+    const collection = db.collection('pais');
+    collection.updateOne({ 'country': req.params.paisId },
+        {
+            $set: {
+                country: req.body.country,
+                population: req.body.population,
+                continent: req.body.continent,
+                lifeExpectancy: req.body.lifeExpectancy,
+                purchasingPower: req.body.purchasingPower
+            }
+        }, (err, result) => {
+            res.send(result);
+        })
+});
+
+app.delete('/paises/:paisId', (req,res) => {
+    const collection = db.collection('pais');
+    collection.deleteOne({'country': req.params.paisId}, (err,result) => {
+        res.send(result);
+    })
+})
+
